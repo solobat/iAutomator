e339a7f843ccecfb79b4ff0e365e3e8f317c1fb2
tomasy <solopea@gmail.com>
2020-06-13 01:01:55 +0800

    bugfix: contextmenu

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -31,61 +31,61 @@ export const BUILDIN_ACTIONS = {
 
 export const BUILDIN_ACTION_CONFIGS = [
   {
-    name: BUILDIN_ACTIONS.READ_MODE,
+    name: 'READ_MODE',
     title: 'Read Mode',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.KILL_ELEMENT,
+    name: 'KILL_ELEMENT',
     title: 'Kill Element',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.HIGHLIGHT_ENGLISH_SYNTAX,
+    name: 'HIGHLIGHT_ENGLISH_SYNTAX',
     title: 'English Syntax highlighting',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.HASH_ELEMENT,
+    name: 'HASH_ELEMENT',
     title: 'Add anchor for elements',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.TIME_UPDATE,
+    name: 'TIME_UPDATE',
     title: 'Add time tag for video',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.DOWNLOAD,
+    name: 'DOWNLOAD',
     title: 'Download element',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.FULL_SCREEN,
+    name: 'FULL_SCREEN',
     title: 'FullScreen element',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.CODE_COPY,
+    name: 'CODE_COPY',
     title: 'Code copy',
     contexts: ['all'],
     asCommand: true
   },
   {
-    name: BUILDIN_ACTIONS.CLICK,
+    name: 'CLICK',
     title: 'Click',
     contexts: ['all'],
     asCommand: false
   },
   {
-    name: BUILDIN_ACTIONS.GOTO_ELEMENT,
+    name: 'GOTO_ELEMENT',
     title: 'Goto element',
     contexts: ['all'],
     asCommand: false
