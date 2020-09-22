818cd17f5f0f1c8be2c36f04cb3ce2311e1e0bb0
tomasy <solopea@gmail.com>
2020-09-22 13:54:34 +0800

    build: v1.1.5

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# prepare
+echo "Prepare dir......"
+
+# build
+echo "Compiling....."
+npm run build || { echo "Compilation failed, please check and try again"; exit 1; }
+
+zip -r ihelpers.zip dist/
+
+echo "Packaged done"
+
+mv -f ihelpers.zip ~/Downloads/
+
+echo "Move to Download"
